3fd94fec7e88c91fa519900bc612f85c0becf44e
Vitaliy Kovalov <vitalya.kovalov@gmail.com>
2024-04-14 22:21:01 +0300

    Add assets

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import smart_lab_logo from './smart_lab_logo.svg';
+import smart_lab from './smart_lab.svg';
+import squares_filled from './squares_filled.svg';
+import devices from './devices.svg';
+import users from './users.svg';
+import user_circle from './user_circle.svg';
+import logout from './logout.svg';
+
+export {
+  smart_lab_logo,
+  smart_lab,
+  squares_filled,
+  devices,
+  users,
+  user_circle,
+  logout,
+};
