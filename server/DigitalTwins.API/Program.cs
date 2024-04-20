f4c81b5e15a45afc51c6e25d4f7f296a4678f22e
Vasyl <59260347+Pavliuk13@users.noreply.github.com>
2024-04-20 19:05:08 +0300

    deploy preparation

--- server/DigitalTwins.API/Program.cs
+++ server/DigitalTwins.API/Program.cs
@@ -31,11 +31,8 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseDigitalTwinContext();
 
