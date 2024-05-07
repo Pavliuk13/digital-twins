49622d220c4aef10bdefc2e983582a9ee7e9cc82
Vasyl <59260347+Pavliuk13@users.noreply.github.com>
2024-05-07 08:20:43 +0300

    encrease azure digital twin url limit

--- server/DigitalTwins.DAL/Migrations/20240507051941_EncreaseUrlLimit.Designer.cs
+++ server/DigitalTwins.DAL/Migrations/20240507051941_EncreaseUrlLimit.Designer.cs
@@ -4,6 +4,7 @@ using DigitalTwins.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DigitalTwins.DAL.Migrations
 {
     [DbContext(typeof(DigitalTwinContext))]
-    partial class DigitalTwinContextModelSnapshot : ModelSnapshot
+    [Migration("20240507051941_EncreaseUrlLimit")]
+    partial class EncreaseUrlLimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/DigitalTwins.DAL/Migrations/20240507051941_EncreaseUrlLimit.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalTwins.DAL.Migrations
+{
+    public partial class EncreaseUrlLimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AzureDigitalTwinUrl",
+                table: "Devices",
+                type: "nvarchar(900)",
+                maxLength: 900,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AzureDigitalTwinUrl",
+                table: "Devices",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(900)",
+                oldMaxLength: 900);
+        }
+    }
+}
